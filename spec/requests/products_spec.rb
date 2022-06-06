067036c6cb6132f39fb2ec4683ee18a9e07f651a
aruns0 <arunmcarit@gmail.com>
2022-06-06 20:09:01 +0530

    updated the test cases

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "Products", type: :request do
   #initialize
-  let!(:products) { create_list(:product, 10) }
+  let!(:products) { create_list(:product, 30) }
   let(:product_id) { products.first.id }
   # Test suite for GET /products
  describe 'GET /products' do
   # make HTTP get request   
   before { get '/api/v1/products?page=1',headers: { 'Authorization' => AuthenticationService.call(ENV["api_key"]) } }
   it 'returns products' do
-    expect(json).not_to be_empty
-    expect(json.size).to eq(ENV["data_limit"].to_i+2)
+    expect(json).not_to be_empty   
+    expect(json["products"].size).to eq(ENV["data_limit"].to_i)
   end
   it 'returns status code 200' do
     expect(response).to have_http_status(200)
@@ -23,8 +23,8 @@ describe 'GET /products/:id' do
     it 'returns status code 200' do
       expect(response).to have_http_status(200)
     end
-    it 'returns the item' do
-      expect(json['id']).to eq(product_id)
+    it 'returns the item' do     
+      expect(json['product']['id']).to eq(product_id)
     end
   end
   context 'when product does not exist' do
